1c84b46bf5465fffc1dd12b002b28b999163e0e1
Sothus <krzysztof.plucinski.95@gmail.com>
2017-08-19 14:22:08 +0200

    Done basic configuration of Django project and created test page.

--- EPCar/EPCar/urls.py
+++ EPCar/EPCar/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from jqmtest.views import TestView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TestView.as_view()),
 ]
