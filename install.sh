732ddd2ee6091ba09a762d57c6e1a1475461a6d8
Sothus <krzysztof.plucinski.95@gmail.com>
2017-10-30 12:17:19 +0100

    Updated install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 sudo apt-get update
-sudo apt-get upgrade
+#sudo apt-get upgrade
 sudo apt-get dist-upgrade
 sudo apt-get install tightvncserver xrdp python3-dev libffi-dev libssl-dev
 sudo pip3 install -U pip
 sudo pip3 install virualenv
-cd /home/pi
-mkdir EpCarProject
-cd EpCarProject/
+sudo cp rc.local /etc/rc.local
+sudo systemctl enable pigpiod.service
 python3 -m venv venv/
 source venv/bin/activate
 pip install -U pip
